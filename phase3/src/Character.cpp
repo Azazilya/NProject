881d9a6f0f9bb812e185edc79fc1c210e1d2664b
Azazilya <novalzakaria177@gmail.com>
2025-07-22 23:29:35 +0700

    day16

--- phase3/src/Character.cpp
+++
@@ -1,6 +0,0 @@
-#include "Character.h"
-
-Character::Character(const string& n) : name(n) {}
-void Character::move() { cout << name << " bergerak ke depan!\n"; }
-void Character::attack() { cout << name << " menyerang musuh!\n"; }
-void Character::defend() { cout << name << " bertahan dengan perisai!\n"; }
